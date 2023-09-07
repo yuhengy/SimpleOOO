3dc453e6bc507614de4caff32b85030e643b455a
Yuheng Yang <yuhengy@mit.edu>
2023-09-06 20:17:27 -0400

    [smallfix]

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,8 @@
 
-# Run everything with `pwd` == TimingTaint or /vagrant
+# Run everything with `pwd` == TheoreticalOOO or /vagrant
 
 
 ## STEP1: Simulate the verilog with verilator
-#  The waveform is generated at `build//OOO-OOO/myVCD.vcd`
+#  The waveform is generated at `build//ISA-ISA/myVCD.vcd`
 # python3 scripts/verilator/run.py src/ISA/ISA.v src/ISA ISA +INIT_RF_CUSTOMIZED+INIT_MEMI_CUSTOMIZED+REG_LEN=4
 
