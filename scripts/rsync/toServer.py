29986ba75275de3f70db547a473b046079298556
Yuheng Yang <yuhengy@mit.edu>
2023-10-14 21:54:29 -0400

    [rsync]

--- scripts/rsync/toServer.py
+++ scripts/rsync/toServer.py
@@ -3,15 +3,16 @@ if __name__ == "__main__":
 
 
   import sys
-  if len(sys.argv) <= 1:
-    print("1 arguments needed!")
-    print("Usage: " + sys.argv[0] + " experimentTag")
+  if len(sys.argv) <= 2:
+    print("2 arguments needed!")
+    print("Usage: " + sys.argv[0] + " server_name experimentTag")
     exit()
-  experimentTag = sys.argv[1]
+  server_name = sys.argv[1]
+  experimentTag = sys.argv[2]
 
   command  = "rsync -av --relative --exclude '__pycache__' --exclude '.DS_Store'"
-  command += " scripts src results/%s mtlsim:PrjTheoreticalOOO/%s" \
-             % (experimentTag, experimentTag)
+  command += " scripts src results/%s %s:PrjTheoreticalOOO/%s" \
+             % (experimentTag, server_name, experimentTag)
   print("[command to run]: ", command)
 
   import os
